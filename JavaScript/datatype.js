419415ef28bdccd885a41327e2d96e2f7cbf0962
shazan99 <ahmedshazan99@gmail.com>
2021-06-06 20:56:43 +0530

    JavaScript Data types
    Signed-off-by: shazan99 <ahmedshazan99@gmail.com>

---
+++ JavaScript/datatype.js
@@ -0,0 +1,15 @@
+function a() {
+    return 1;
+}
+
+
+
+console.log(typeof 13333)
+console.log(typeof 23335478.3325)
+console.log('a', typeof 'a')
+console.log('aaasdjbek',typeof 'aaasdjbek')
+console.log(true, typeof true)
+console.log(null , typeof null)
+console.log(undefined, typeof undefined)
+console.log(a, typeof a)
+console.log(a(), typeof a())
