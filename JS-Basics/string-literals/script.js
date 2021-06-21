db32f5b1c3d4d20942d75836fdd64c3419254f78
shazan99 <ahmedshazan99@gmail.com>
2021-06-21 19:55:37 +0530

    string literals
    Signed-off-by: shazan99 <ahmedshazan99@gmail.com>

---
+++ JS-Basics/string-literals/script.js
@@ -0,0 +1,20 @@
+let s1 = 'He said ,"This is awesome!"'
+
+let s2= "He said,\"This is awesome!\""
+
+let s3="He said, 'This is awesome!'"
+
+let s4= `He said , 'This is awesome!'`
+
+let s5= "This string is \n in multiple lines"
+
+let s6= `
+This is a GOOd Morning Message`
+
+let name= 'John'
+
+let g1= 'Good Morning ' + name
+
+let g2 = `Good Morning ${name}` 
+
+let s7 = `10+2 = ${10+2}`
