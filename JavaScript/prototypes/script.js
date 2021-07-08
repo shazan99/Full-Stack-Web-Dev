7bdd9b14ca177aee7738fbefcfeafb178344d0ba
shazan99 <ahmedshazan99@gmail.com>
2021-07-08 13:08:27 +0530

    Prototypes in JS
    Signed-off-by: shazan99 <ahmedshazan99@gmail.com>

---
+++ JavaScript/prototypes/script.js
@@ -0,0 +1,16 @@
+let str = "Asdasd"
+let num = 233
+let bool = true
+let arr = [255,65,48,69,654]
+let obj = {a:10, b: 'asdasd'}
+
+// if x and y are not primitive
+// x==y : true <- what does this mean ?
+// this means they are same object in memory
+
+console.log('typeof String',typeof String)
+console.log('typeof Object',typeof Object)
+console.log('typeof Boolean',typeof Boolean)
+console.log('typeof Number',typeof Number)
+console.log('typeof Array',typeof Array)
+console.log('typeof Function',typeof Function)
